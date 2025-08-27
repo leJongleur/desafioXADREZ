207d884186e42a6df932739efb6284ef38f81812
leJongleur <gomescarneiro@rocketmail.com>
2025-08-26 23:35:46 -0300

    Add files via upload

--- XADREZdesafio.c
+++ XADREZdesafio.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main (){
+    int i;
+
+    //Bispo: 5 casas na diagonal (Cima + Direita)
+    printf("Bispo:\n");
+    for (i = 0; i < 5; i++) {
+        printf("Cima\n");
+        printf("Direita\n");
+    }
+    printf("\n");
+
+    // Torre: 5 casas para direita
+    printf("Torre:\n");
+    i = 0;
+    while (i < 5) {
+        printf("Direita\n");
+        i++;
+    }
+    printf("\n");
+
+    // Rainha: 8 casas para a esquerda
+    printf("Rainha:\n");
+    i = 0;
+    do {
+        printf("Esquerda\n");
+        i++;
+    } while (i < 8);
+
+    return 0;
+}
